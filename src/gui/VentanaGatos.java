db87f2aa62d36017528f30e3e71f3050d44672f3
Mireia Otaduy <mireia.otaduy@opendeusto.es>
2025-10-26 17:08:08 +0100

    ventanagatos + caracteristicas

--- src/gui/VentanaGatos.java
+++ src/gui/VentanaGatos.java
@@ -1,15 +1,23 @@
 package gui;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -27,9 +35,11 @@ import Domain.Gato;
 public class VentanaGatos extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-
+	private Gato[] gatos;
 
 	public VentanaGatos(Gato[] gatos) {
+		
+		this.gatos = gatos;
 
 		setTitle("GATOS");
 		setSize(850, 600);
@@ -108,41 +118,45 @@ public class VentanaGatos extends JFrame {
 			paneles[i].add(boton);
 		}
 
-		//---------------------------------------------------------------------------------------------------------------------------------------
-
-		//-----------------------------------------------VENTANA CARACTERISTICAS GATO--------------------------------------------------------------
-
-		//panel que se abre despues de darle al boton 'ver mas' para ver mejor las caracteristicas de cada gato
-
-		//crear ventana y panel
-		JFrame ventanaCarac = new JFrame("Características del gato");
-		ventanaCarac.setSize(500, 400);
-		ventanaCarac.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		ventanaCarac.setLocationRelativeTo(null);
-
-		JPanel panelVer = new JPanel();
-
-		ventanaCarac.add(panelVer);
-
-		//listener boton
+		//----------LISTENER---------
 		for(int i = 0; i < 12; i++) {
-			botones[i].addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					ventanaCarac.add(panelVer);
-					ventanaCarac.setVisible(true);
-
-				}
-
-			});
+		    final int indiceGato = i; // 
+		    botones[i].addActionListener(new ActionListener() {
+		        @Override
+		        public void actionPerformed(ActionEvent e) {
+		            mostrarCaracteristicas(indiceGato);
+		        }
+		    });
 		}
 
 		frame.add(jscroll);
 		add(frame);
 		setVisible(true);
 
+	}
+
+	//funcion para el perfil redondo
+	private BufferedImage createCircleImage(String imagePath, int diameter, Color borderColor) {
+		try {
+			BufferedImage originalImage = ImageIO.read(new File(imagePath)); // carga la imagen desde el archivo
+			Image scaledImage = originalImage.getScaledInstance(diameter, diameter, Image.SCALE_SMOOTH); // redimensiona suavemente
+			BufferedImage circularImage = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB); // crea imagen transparente para dibujar el círculo
+
+			Graphics2D g = circularImage.createGraphics(); // obtiene el contexto gráfico
+			g.setClip(new Ellipse2D.Float(0, 0, diameter, diameter)); // define la región circular de recorte
+			g.drawImage(scaledImage, 0, 0, null); // dibuja la imagen escalada dentro del círculo
+
+			g.setClip(null); // desactiva el recorte para poder dibujar el borde
+			g.setColor(borderColor); // establece el color del borde
+			g.setStroke(new BasicStroke(3)); // define el grosor del borde
+			g.drawOval(0, 0, diameter - 1, diameter - 1); // dibuja el borde circular
+			g.dispose(); // libera recursos del objeto Graphics2D
+
+			return circularImage; // devuelve la imagen circular resultante
+		} catch (IOException e) {
+			System.out.println("Error al cargar la imagen: " + e.getMessage()); // muestra error si falla la carga
+			return null; // devuelve null en caso de error
+		}
 	}
 
 	//funcion para crear imagen
@@ -154,6 +168,93 @@ public class VentanaGatos extends JFrame {
 		JLabel label = new JLabel(icono);
 		label.setAlignmentX(Component.CENTER_ALIGNMENT); // para BoxLayout vertical
 		return label;
+	}
+
+	//funcion para mostrar caracteristicas de cada gato
+	private void mostrarCaracteristicas(int indiceGato) {
+
+		//-----------------------------------------------VENTANA CARACTERISTICAS GATO--------------------------------------------------------------
+
+		//panel que se abre despues de darle al boton 'ver mas' para ver mejor las caracteristicas de cada gato
+
+		//----------FRAME-----------
+		JFrame ventanaCarac = new JFrame("Características del gato");
+		ventanaCarac.setSize(500, 400);
+		ventanaCarac.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		ventanaCarac.setLocationRelativeTo(null);
+		ventanaCarac.getContentPane().setLayout(new BoxLayout(ventanaCarac.getContentPane(), BoxLayout.Y_AXIS));
+
+		//añadir componentes con caracteristicas
+
+		//---------PANEL PERFIL-----------
+
+		JPanel panelPerfil = new JPanel();
+		panelPerfil.setPreferredSize(new Dimension(500, 120));
+		panelPerfil.setBackground(new Color(255, 182, 193));
+
+		ImageIcon fotoPerfilGato = new ImageIcon(createCircleImage("imagenes/gatos/gato" + (indiceGato+1) + ".png", 90, new Color(255, 0, 127)));
+		JLabel fotoLabelGato = new JLabel(fotoPerfilGato);
+		fotoLabelGato.setHorizontalAlignment(JLabel.CENTER);
+		panelPerfil.add(fotoLabelGato);
+
+		panelPerfil.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createLineBorder(new Color(255, 0, 127)), 
+				"miau", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(255, 0, 127)
+				));
+
+
+		//---------PANEL INFO----------- FALTA PONER BIEN LAS ESTIQUETAS
+		JPanel panelInfo = new JPanel();
+		panelInfo.setPreferredSize(new Dimension(500, 280));
+		panelInfo.setBackground(new Color(245, 245, 220));
+		panelInfo.setLayout(new BorderLayout(20, 20));
+
+		Gato gato = gatos[indiceGato];
+		
+		JLabel nombreGato = new JLabel(gato.getNombre(), JLabel.CENTER);
+		nombreGato.setFont(new Font("Verdana", Font.BOLD, 36));
+		panelInfo.add(nombreGato, BorderLayout.NORTH);
+		
+		JButton botonAdopt = new JButton("Adóptame");
+		botonAdopt.setPreferredSize(new Dimension(100, 30)); // ancho x alto
+		botonAdopt.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panelInfo.add(botonAdopt, BorderLayout.SOUTH);
+		
+		//para poner el boton pequeño y centrado
+		JPanel contenedorBoton = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		contenedorBoton.setBackground(new Color(245, 245, 220));
+		contenedorBoton.add(botonAdopt);
+		panelInfo.add(contenedorBoton, BorderLayout.SOUTH);
+		
+		//panel grid para añadir datos que faltan
+		JPanel panelDatos = new JPanel();
+		panelDatos.setLayout(new FlowLayout());
+		panelDatos.setBackground(new Color(245, 245, 220));
+		
+		JPanel panelDatos1 = new JPanel();
+		panelDatos1.setLayout(new GridLayout(2, 4, 12, 12));
+		panelDatos1.setPreferredSize(new Dimension(500, 60));
+		panelDatos1.setBackground(new Color(245, 245, 220));
+		panelDatos1.add(new JLabel("<html><b>Edad</b>: " + gatos[indiceGato].getEdad() + "años"));
+		panelDatos1.add(new JLabel("<html><b>Sexo</b>: " + gatos[indiceGato].getSexo()));
+		panelDatos1.add(new JLabel("<html><b>Peso</b>: " + gatos[indiceGato].getPeso() + "kg"));
+		panelDatos1.add(new JLabel("<html><b>Raza</b>: " + gatos[indiceGato].getRaza()));
+		
+		JPanel panelDatos2 = new JPanel();
+		panelDatos2.setLayout(new BoxLayout(panelDatos2, BoxLayout.Y_AXIS));
+		panelDatos2.setPreferredSize(new Dimension(500, 60));
+		panelDatos2.setBackground(new Color(245, 245, 220));
+		panelDatos2.add(new JLabel("<html><b>Personalidad</b>: " + gatos[indiceGato].getDescripcion_personalidad()));
+		panelDatos2.add(new JLabel("<html><b>Rasgos</b>: " + gatos[indiceGato].getDescripcion_fisica()));
+		
+		//añadir paneles al frame
+		panelDatos.add(panelDatos1);
+		panelDatos.add(panelDatos2);
+		panelInfo.add(panelDatos, BorderLayout.CENTER);
+		ventanaCarac.add(panelPerfil);
+		ventanaCarac.add(panelInfo);
+		ventanaCarac.setVisible(true);
+
 	}
 
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import Domain.Gato;
+import Domain.Pajaro;
+import Domain.Perro;
+import Domain.Roedor;
 import gui.VentanaGatos;
 
 public class Main {
@@ -10,19 +13,23 @@ public class Main {
 		
 		//-------------------GATOS------------------
 		Gato[] gatos = new Gato[12];
+		Perro[] perros = new Perro[12];
+		Roedor[] roedores = new Roedor[12];
+		Pajaro[] pajaros = new Pajaro[12];
 		
-		Gato gato1 = new Gato("Misu", "hembra", 1.5f, "Bengala", 4.2f, "Curiosa, atenta y cariñosa", "Pelaje corto, atigrado, manchas tipo leopardo, ojos grandes verdes", false);
-		Gato gato2 = new Gato("Nieve", "hembra", 3.0f, "Europeo", 4.5f, "Tranquila y observadora", "Pelaje blanco, ojos verdes, tamaño mediano", false);
-		Gato gato3 = new Gato("Tris", "hembra", 2.0f, "Carey", 4.0f, "Curiosa, serena y elegante", "Pelaje tricolor con manchas naranjas, negras y blancas; ojos grandes verdes; orejas puntiagudas", false);
-		Gato gato4 = new Gato("Luna", "hembra", 2.5f, "Atigrado/Carey", 4.1f, "Activa, curiosa y cariñosa", "Pelaje tricolor con mezcla de naranja, negro y crema; ojos amarillos grandes", false);
-		Gato gato5 = new Gato("Copito", "macho", 4.0f, "Persa bicolor", 5.2f, "Sociable, tranquilo y dulce", "Pelaje largo blanco con manchas naranjas, ojos claros redondos, cuerpo robusto", false);
-		Gato gato6 = new Gato("Maya", "hembra", 3.0f, "Siamés tricolor", 3.7f, "Inteligente, tranquila y sociable", "Pelaje corto claro con manchas oscuras en cara, patas y orejas; ojos amarillos; rostro mitad oscuro", false);
-		Gato gato7 = new Gato("Tigre", "macho", 2.8f, "Bengala", 4.4f, "Enérgico, juguetón y curioso", "Pelaje largo marrón con rayas oscuras y patrón tipo tigre, ojos marrón dorado", false);
-		Gato gato8 = new Gato("Olaf", "macho", 2.0f, "Europeo", 4.0f, "Juguetón, amistoso y curioso", "Pelaje atigrado marrón con blanco, ojos de diferente color (uno azul y uno ámbar), patas blancas", false);
-		Gato gato9 = new Gato("Nube", "hembra", 2.5f, "Europeo", 3.8f, "Tímida, inteligente y curiosa", "Pelaje corto atigrado marrón con tonos anaranjados, ojos azules", false);
-		Gato gato10 = new Gato("Leo", "macho", 4.5f, "Siberiano", 5.3f, "Tranquilo, independiente y elegante", "Pelaje largo color crema y gris, ojos verdes, tamaño grande", false);
-		Gato gato11 = new Gato("Shadow", "macho", 3.2f, "Bombay", 4.3f, "Observador, tranquilo y leal", "Pelaje corto negro con reflejos marrón, ojos amarillos intensos", false);
-		Gato gato12 = new Gato("Simba", "macho", 3.5f, "Europeo", 4.6f, "Sociable, juguetón y amigable", "Pelaje corto naranja atigrado, ojos verdes, cuerpo atlético", false);
+		Gato gato1 = new Gato("Misu", "hembra", 1.5f, "Bengala", 4.2f, "Curiosa, atenta y cariñosa", "Atigrado, manchas tipo leopardo, ojos verdes", false);
+		Gato gato2 = new Gato("Nieve", "hembra", 3.0f, "Europeo", 4.5f, "Tranquila y observadora", "Pelaje blanco, ojos verdes", false);
+		Gato gato3 = new Gato("Tris", "hembra", 2.0f, "Carey", 4.0f, "Curiosa, serena y elegante", "Tricolor, ojos verdes", false);
+		Gato gato4 = new Gato("Luna", "hembra", 2.5f, "Atigrado/Carey", 4.1f, "Activa, curiosa y cariñosa", "Tricolor, ojos amarillos", false);
+		Gato gato5 = new Gato("Copito", "macho", 4.0f, "Persa bicolor", 5.2f, "Sociable, tranquilo y dulce", "Blanco con manchas naranjas, ojos claros", false);
+		Gato gato6 = new Gato("Maya", "hembra", 3.0f, "Siamés tricolor", 3.7f, "Inteligente, tranquila y sociable", "Claro con manchas oscuras, ojos amarillos", false);
+		Gato gato7 = new Gato("Tigre", "macho", 2.8f, "Bengala", 4.4f, "Enérgico, juguetón y curioso", "Marrón con rayas oscuras, ojos dorados", false);
+		Gato gato8 = new Gato("Olaf", "macho", 2.0f, "Europeo", 4.0f, "Juguetón, amistoso y curioso", "Atigrado marrón y blanco, ojos azul y ámbar", false);
+		Gato gato9 = new Gato("Nube", "hembra", 2.5f, "Europeo", 3.8f, "Tímida, inteligente y curiosa", "Atigrado marrón, ojos azules", false);
+		Gato gato10 = new Gato("Leo", "macho", 4.5f, "Siberiano", 5.3f, "Tranquilo, independiente y elegante", "Crema y gris, ojos verdes", false);
+		Gato gato11 = new Gato("Shadow", "macho", 3.2f, "Bombay", 4.3f, "Observador, tranquilo y leal", "Negro, ojos amarillos", false);
+		Gato gato12 = new Gato("Simba", "macho", 3.5f, "Europeo", 4.6f, "Sociable, juguetón y amigable", "Naranja atigrado, ojos verdes", false);
+
 
 		gatos[0]  = gato1;
 		gatos[1]  = gato2;
